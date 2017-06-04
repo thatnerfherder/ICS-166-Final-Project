7c0903421ecb995e452feacf7aee4453355bab47
thatnerfherder <jlamorte@uci.edu>
2017-06-04 15:27:08 -0700

    add test first scene
    first scene allows you to switch between tabs and currently that is it.

---
+++ Haunted Network/Assets/Tabs.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tabs : MonoBehaviour {
+
+    public Image essay;
+    public Image email;
+    public Image eee;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   public void updateEssay()
+    {
+        essay.enabled = true;
+        email.enabled = false;
+        eee.enabled = false;
+    }
+    
+   public void updateEmail()
+    {
+        essay.enabled = false;
+        email.enabled = true;
+        eee.enabled = false;
+    }
+
+    public void updateEEE()
+    {
+        essay.enabled = false;
+        email.enabled = false;
+        eee.enabled = true;
+    }
+}
