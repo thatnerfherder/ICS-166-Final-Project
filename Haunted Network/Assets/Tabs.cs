ea7ce749a19079d64f15773b9744134279ddfa1c
ZotBuster <kdlennon@uci.edu>
2017-06-06 00:35:40 -0700

    Added Scene 2 Transition
    Hitting the upload button now sends you to a restart screen. After 400
    frames the computer "restarts" with a virus pop up window which goes
    away when clicked. Added scene int variable to say what scene we're in.
    Still need sound effects. Virus popup is a placeholder image for now.

--- Haunted Network/Assets/Tabs.cs
+++ Haunted Network/Assets/Tabs.cs
@@ -3,30 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Tabs : MonoBehaviour {
+public class Tabs : MonoBehaviour
+{
 
     public Image essay;
     public Image email;
     public Image eee;
+    public Image restart;
+    public Image virusAlert;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int sceneNumber;
 
-   public void updateEssay()
+    // Use this for initialization
+    void Start()
+    {
+        sceneNumber = 1;
+        restart.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void updateEssay()
     {
         essay.enabled = true;
         email.enabled = false;
         eee.enabled = false;
     }
-    
-   public void updateEmail()
+
+    public void updateEmail()
     {
         essay.enabled = false;
         email.enabled = true;
@@ -38,5 +46,23 @@ public class Tabs : MonoBehaviour {
         essay.enabled = false;
         email.enabled = false;
         eee.enabled = true;
+    }
+
+    public void uploadButtonOnClick()
+    {
+        if (sceneNumber == 1)
+        {
+            essay.enabled = false;
+            email.enabled = false;
+            eee.enabled = false;
+            restart.enabled = true;
+        }
+    }
+    public void virusButtonOnClick()
+    {
+        if (sceneNumber == 2 && virusAlert.enabled == true)
+        {
+            virusAlert.enabled = false;
+        }
     }
 }
---
+++ Haunted Network/Assets/restartScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class restartScript : MonoBehaviour
+{
+
+    public Image essay;
+    public Image email;
+    public Image eee;
+    public Image restart;
+    public Image virusAlert;
+    public int restartTimer;
+
+
+    public int sceneNumber;
+
+    // Use this for initialization
+    void Start()
+    {
+        restartTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (restart.enabled == true)
+        {
+            if (restartTimer <= 400)
+            {
+                restartTimer++;
+            }
+            else
+            {
+                restart.enabled = false;
+                email.enabled = true;
+                virusAlert.enabled = true;
+                restartTimer = 0;
+                GameObject gameMaster = GameObject.Find("GameMaster");
+                Tabs tabs = gameMaster.GetComponent<Tabs>();
+                tabs.sceneNumber = 2;
+            }
+        }
+    }
+}
